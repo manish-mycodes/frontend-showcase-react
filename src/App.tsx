75a56de8ce06b00ac17bc4a4036dd52f04badfa1
Manish Kumar <manish.mycodes@gmail.com>
2025-03-08 23:33:23 +0530

    Upgraded to React 19

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
-export default function App() {
-    const arr = Array.from({ length: 5 }, (v, i) => i)
 
-    console.log('arr')
-    console.log(arr)
-
-    return <div>Hi! This is react</div>
+const App: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the React Showcase</h1>
+        </div>
+    )
 }
+
+export default App
